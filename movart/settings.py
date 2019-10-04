c075aa06ecc90f1e3a767d9ed037e15fd346e515
Default Profile <gabriel.hruza@gmail.com>
2019-10-03 23:35:11 -0300

    credenciales de instagram

--- movart/settings.py
+++ movart/settings.py
@@ -11,3 +11,9 @@ DATABASES = {
         default=config('DATABASE_URL')
    )
 }
+
+# Instagram
+
+SOCIAL_AUTH_INSTAGRAM_KEY = 'bafa9911f3794994a6d8c1cc8bdbdd5b'      #Client ID
+SOCIAL_AUTH_INSTAGRAM_SECRET = '0d88a3202a87490ab9c74097d40b7027'   #Client SECRET
+SOCIAL_AUTH_INSTAGRAM_EXTRA_DATA = [('user', 'user'),]
