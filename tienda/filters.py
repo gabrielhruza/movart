02feb6ed9e8ab267c8d47a8ab0e13c05f2182ff5
Default Profile <gabriel.hruza@gmail.com>
2019-10-07 11:14:21 -0300

    filtro icontains

--- tienda/filters.py
+++ tienda/filters.py
@@ -4,6 +4,7 @@ from .models import Producto
 
 
 class ProductoFilter(django_filters.FilterSet):
+    nombre 		= django_filters.CharFilter(lookup_expr='icontains')       
     class Meta:
         model 	= Producto
-        fields 	= ['nombre', 'precio']
+        fields = ['nombre', 'precio']
