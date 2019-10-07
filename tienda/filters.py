8cf4d3ad2fa6bf33738521e1aa13bbba4b431050
Default Profile <gabriel.hruza@gmail.com>
2019-10-07 11:50:08 -0300

    etiquetas field

--- tienda/filters.py
+++ tienda/filters.py
@@ -5,6 +5,7 @@ from .models import Producto
 
 class ProductoFilter(django_filters.FilterSet):
     nombre 		= django_filters.CharFilter(lookup_expr='icontains')       
+    etiquetas	= django_filters.CharFilter(lookup_expr='name__in')       
     class Meta:
         model 	= Producto
-        fields = ['nombre', 'precio']
+        fields = ['nombre', 'precio', 'etiquetas']
