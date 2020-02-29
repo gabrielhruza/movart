b2630dcb2766a0977d6d1ab1df07354fee2c4493
Hruza Gabriel Carlos <gabriel.hruza@gmail.com>
2020-02-29 20:13:05 -0300

    update link para denunciar y ver denuncias

--- denuncia/urls.py
+++ denuncia/urls.py
@@ -5,6 +5,6 @@ from . 				import views
 app_name = 'denuncia'
 
 urlpatterns = [
-	path('list/', views.dlist, name='denuncia_list'),
-	path('add/<pid>', views.dadd, name='denuncia_add'),
+	path('list/', views.dlist, name='dlist'),
+	path('add/<pid>', views.dadd, name='dadd'),
 ]
