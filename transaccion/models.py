717a8259d312583291dc65700ae4f72a58ca3dd3
Hruza Gabriel Carlos <gabriel.hruza@gmail.com>
2020-03-01 22:30:22 -0300

    migrate movimiento

--- transaccion/models.py
+++ transaccion/models.py
@@ -16,3 +16,21 @@ class Transaccion(models.Model):
 	def __str__(self):
 		return self.cliente.username
 
+
+class Movimiento(models.Model):
+	LISTO 		= 1
+	CONFIRMADO 	= 2
+	DESPACHADO 	= 3
+	RECIBIDO 	= 4
+	CANCELADO	= 5
+	SUSPENDIDO   = 6
+	ESTADO = (
+		(LISTO, 'Listo'),
+		(CONFIRMADO, 'Cofirmado'),
+		(DESPACHADO, 'Despachado'),
+		(RECIBIDO, 'Recibido'),
+		(CANCELADO, 'Cancelado'),
+		(SUSPENDIDO, 'Suspendido'),
+	)
+	transaccion = models.ForeignKey(Transaccion, on_delete=models.CASCADE, default=None) 	
+	estado = models.PositiveSmallIntegerField( choices=ESTADO, default=LISTO ,)
