42b573cb9bce72f1a3fc2d5e4e7ecf70ca323e1d
Hruza Gabriel Carlos <gabriel.hruza@gmail.com>
2021-11-14 12:37:44 -0300

    add import image

--- impinstagram/views.py
+++ impinstagram/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+import secrets
 
 @login_required
 def feed(request):
 	t = 'Importa tu contenido de Instagram'
-	username = request.user.username			
-	return render(request, 'impinstagram/impinstagram.html', {'username' : username, 'titulo': t})
+	username = request.user.username		
+	recent_media = []	
+	for i in range(7):
+		shortcode       = secrets.token_urlsafe(16)
+		thumbnail_src	= str(i) + ".jpg"
+		media 			= dict({'shortcode': shortcode, 'thumbnail_src':thumbnail_src})
+		recent_media.append(media) 
+
+	return render(request, 'impinstagram/impinstagram.html', {'username' : username, 'titulo': t, 'recent_media': recent_media})
 
 
